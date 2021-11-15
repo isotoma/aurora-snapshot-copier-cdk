6a78f62eca43f579c1b34d3a6cbd6868b8fc23f2
Andrew Plummer <plummer574@gmail.com>
2021-11-15 09:20:06 +0000

    Test for the CDK construct

---
+++ test/cdk.test.ts
@@ -0,0 +1,33 @@
+import { expect as cdkExpect, haveResource } from '@aws-cdk/assert';
+import { Stack } from '@aws-cdk/core';
+import '@aws-cdk/assert/jest';
+
+import { AuroraSnapshotCopier } from '..';
+
+describe('construct', () => {
+    test('simple, dry run', () => {
+        const stack = new Stack();
+
+        new AuroraSnapshotCopier(stack, 'Copier', {
+            sources: [
+                {
+                    dbClusterIdentifier: 'myidentifier',
+                },
+            ],
+            target: {
+                regions: ['eu-west-1', 'eu-central-1'],
+            },
+        });
+
+        cdkExpect(stack).to(
+            haveResource('AWS::Lambda::Function', {
+                Environment: {
+                    Variables: {
+                        SOURCE_0_DB_CLUSTER_IDENTIFIER: 'myidentifier',
+                        TARGET_REGIONS: 'eu-west-1,eu-central-1',
+                    },
+                },
+            }),
+        );
+    });
+});
